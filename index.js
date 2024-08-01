a9e12239694a445ef6dba71bc7b43991dc638433
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-08-01 22:56:15 +0100

    SETUP URLS5535555

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.use(errorHandler);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Server');
+  res.json(process.env);
 
 
 });
--- utils/socket.js
+++ utils/socket.js
@@ -1,43 +1,43 @@
-// socketHandler.js
-const { origin,socketPort } = require('./port.js');
-const User = require('../models/User');
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose')
+// // socketHandler.js
+// const { origin,socketPort } = require('./port.js');
+// const User = require('../models/User');
+// const express = require('express');
+// const app = express();
+// const mongoose = require('mongoose')
 
-// mongoose.connect(process?.env?.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/SheeshTV').then(() => console.log('DB connected')).catch(err => console.log(err));
+// // mongoose.connect(process?.env?.MONGODB_URI ?? 'mongodb://127.0.0.1:27017/SheeshTV').then(() => console.log('DB connected')).catch(err => console.log(err));
 
-const http = require('http')
-const socketIo = require('socket.io');
-
-
-const server = http.createServer(app)
-
-const io = socketIo(server, {
-  cors: {
-    origin: origin,
-    methods: ['GET', 'POST']
-  }
-})
+// const http = require('http')
+// const socketIo = require('socket.io');
 
 
+// const server = http.createServer(app)
 
-io.on('connection', (socket) => {
-  socket.on('register', async (userId) => {
-    const user = await User.findById(userId);
-    if (user) {
-      user.socketId = socket.id;
-      await user.save();
-    }
-  });
+// const io = socketIo(server, {
+//   cors: {
+//     origin: origin,
+//     methods: ['GET', 'POST']
+//   }
+// })
 
-  socket.on('disconnect', async () => {
-    const user = await User.findOne({ socketId: socket.id });
-    if (user) {
-      user.socketId = null;
-      await user.save();
-    }
-  });
-});
 
-io.listen(socketPort, () => console.log(`Server Start port ${socketPort}`));
+
+// io.on('connection', (socket) => {
+//   socket.on('register', async (userId) => {
+//     const user = await User.findById(userId);
+//     if (user) {
+//       user.socketId = socket.id;
+//       await user.save();
+//     }
+//   });
+
+//   socket.on('disconnect', async () => {
+//     const user = await User.findOne({ socketId: socket.id });
+//     if (user) {
+//       user.socketId = null;
+//       await user.save();
+//     }
+//   });
+// });
+
+// io.listen(socketPort, () => console.log(`Server Start port ${socketPort}`));
