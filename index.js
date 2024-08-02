e59164049c6cd84e9b467113ce72924adedf317f
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-08-02 19:30:35 +0100

    ff

--- index.js
+++ index.js
@@ -30,12 +30,12 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://soft-uni-project-sheesh-tv.vercel.app');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
+app.use(cors(corsOptions));
 app.use(auth);
 app.use('/data/videos', express.static(path.join(__dirname, 'videos')));
 app.use('/users/avatar', express.static(path.join(__dirname, 'avatar')));
