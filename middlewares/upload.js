0f3f0d59a92fe571ae1f0d0bcf7f933c640607b8
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-08-03 03:17:00 +0100

    upfawfwuykop

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,37 +1,39 @@
+
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+const path = require('path');
 require('dotenv').config();
 
-console.log('Bucket Name:', process.env.BUCKET_NAME);
-console.log('Bucket Region:', process.env.BUCKET_REGION);
-console.log('Access Key ID:', process.env.ACCESS_KEY);
-console.log('Secret Access Key:', process.env.SECRET_ACCESS_KEY);
+const bucketName =process.env.BUCKET_NAME
+const bucketRegion=process.env.BUCKET_REGION
+const accessKeyId =process.env.ACCESS_KEY
+const secretAccessKey =process.env.SECRET_ACCESS_KEY
 
-const bucketName = process.env.BUCKET_NAME;
-const bucketRegion = process.env.BUCKET_REGION;
-const accessKeyId = process.env.ACCESS_KEY;
-const secretAccessKey = process.env.SECRET_ACCESS_KEY;
+
 
 aws.config.update({
     accessKeyId: accessKeyId,
     secretAccessKey: secretAccessKey,
-    region: bucketRegion,
+    region:bucketRegion,
 });
 
 const s3 = new aws.S3();
-console.log('AWS S3 Configuration:', s3.config);
+console.log(s3);
 
 const storage = multerS3({
     s3: s3,
     bucket: bucketName,
-    acl: 'public-read',
+    acl: 'public-read', 
     key: function (req, file, cb) {
         cb(null, `videos/${Date.now()}_${path.basename(file.originalname)}`);
     }
 });
 
-const upload = multer({
+const upload = multer({ 
     storage: storage,
     limits: {
-        fileSize: 20000000
+        fileSize: 20000000 
     }
 });
 
