71cc836ecf67c961d403fc652ddb154af23568f7
pehlo2 <132269171+pehlo2@users.noreply.github.com>
2024-08-03 02:59:10 +0100

    update url 23354fwf

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,40 +1,40 @@
 
-// const multer = require('multer');
-// const multerS3 = require('multer-s3');
-// const aws = require('aws-sdk');
-// const path = require('path');
-// require('dotenv').config();
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+const path = require('path');
+require('dotenv').config();
 
-// const bucketName =process.env.BUCKET_NAME
-// const bucketRegion=process.env.BUCKET_REGION
-// const accessKeyId =process.env.ACCESS_KEY
-// const secretAccessKey =process.env.SECRET_ACCESS_KEY
+const bucketName =process.env.BUCKET_NAME
+const bucketRegion=process.env.BUCKET_REGION
+const accessKeyId =process.env.ACCESS_KEY
+const secretAccessKey =process.env.SECRET_ACCESS_KEY
 
 
 
-// aws.config.update({
-//     accessKeyId: accessKeyId,
-//     secretAccessKey: secretAccessKey,
-//     region:bucketRegion,
-// });
+aws.config.update({
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey,
+    region:bucketRegion,
+});
 
-// const s3 = new aws.S3();
-// console.log(s3);
+const s3 = new aws.S3();
+console.log(s3);
 
-// const storage = multerS3({
-//     s3: s3,
-//     bucket: bucketName,
-//     acl: 'public-read', 
-//     key: function (req, file, cb) {
-//         cb(null, `videos/${Date.now()}_${path.basename(file.originalname)}`);
-//     }
-// });
+const storage = multerS3({
+    s3: s3,
+    bucket: bucketName,
+    acl: 'public-read', 
+    key: function (req, file, cb) {
+        cb(null, `videos/${Date.now()}_${path.basename(file.originalname)}`);
+    }
+});
 
-// const upload = multer({ 
-//     storage: storage,
-//     limits: {
-//         fileSize: 20000000 
-//     }
-// });
+const upload = multer({ 
+    storage: storage,
+    limits: {
+        fileSize: 20000000 
+    }
+});
 
-// module.exports = upload;
+module.exports = upload;
